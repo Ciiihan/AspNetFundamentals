95116ddb4a82c6a1a2c29ba0ad09eccf18e37d3a
Ciiihan <61598368+Ciiihan@users.noreply.github.com>
2020-11-28 00:18:11 +0300

    Add project files.

---
+++ Asp.netCore3.1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCore3._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index ()
+        //{
+        //    return "İlk Görünüm";
+        //}
+        public IActionResult About ()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspCoreElKitabi/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreElKitabi.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public Decimal? Price { get; set; }
+        public static Product [] GetProducts()
+        {
+            Product bilgisayar = new Product
+            {
+                Name="Bilgisayar" , Price=6500M
+            };
+            Product monitor = new Product
+            {
+                Name = "Monitor",
+                Price = 3500M
+            };
+            return new Product[] { bilgisayar, monitor, null };
+        }
+    }
+
+}
